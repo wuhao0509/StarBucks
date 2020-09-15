c7853c311c909bbb1dea44e78eecd07fdba7c4ca
wuhao0509 <303459939@qq.com>
2020-09-15 16:50:59 +0800

    实现页面右侧功能

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-$('#showCon').on('click',function(){
+$('#showCon').on('click', function () {
     $('.menu').removeClass('hide').addClass('show');
+    shadowMenu();
 })
-$('#closeBtn').on('click',function(){
+$('#closeBtn').on('click', function () {
     $('.menu').removeClass('show').addClass('hide');
+    shadowMenu();
 })
+
+//只要屏幕尺寸改变就会触发shadowMenu()
+$(window).resize(shadowMenu);
+
+//当菜单弹出的时候，不要让body出现滚动条。当菜单消失的时候再让body出现滚动条
+function shadowMenu() {
+    //只有当屏幕尺寸小于992，以及menu菜单是显示的状态，这时才要干掉body的滚动条
+    if ($(window).innerWidth() <= 992 && $('.menu').hasClass('show')) {
+        //console.log(1);
+        $('body').css('overflow', 'hidden');
+    } else {
+        $('body').css('overflow', 'auto');
+    }
+}
